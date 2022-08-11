c4034805bbcd1afa5bc7c46fdaab7771ddbd0464
Łukasz Kolber <69080157+psp515@users.noreply.github.com>
2022-08-11 13:55:55 +0200

    Time: 33 ms (42.07%), Space: 13.3 MB (64.22%) - LeetHub

---
+++ 27-remove-element/27-remove-element.py
@@ -0,0 +1,25 @@
+
+import heapq
+
+class Solution(object):
+    
+    def removeElement(self, nums, val):
+        
+        n = len(nums)
+        i,j = 0, n - 1
+        
+        while i < j:
+            if nums[i] == val :
+                nums[i], nums[j] = nums[j], nums[i]
+                j -= 1
+            else:
+                i += 1
+        
+        count = 0
+        
+        for element in nums:
+            if element != val:
+                count +=1
+        
+        return count
+        
