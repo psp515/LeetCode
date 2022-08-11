337cd9b741026e7d0623afaf631540e395ddeb82
Łukasz Kolber <69080157+psp515@users.noreply.github.com>
2022-08-11 14:40:39 +0200

    Time: 225 ms (50.42%), Space: 14.6 MB (83.61%) - LeetHub

--- 283-move-zeroes/283-move-zeroes.py
+++ 283-move-zeroes/283-move-zeroes.py
@@ -2,13 +2,13 @@ class Solution(object):
     
     def moveZeroes(self, nums):
         count = 0 
-        n = len(nums)
-        for i in range(n):
+        
+        for i in range(len(nums)):
             if nums[i] != 0:
                 nums[count] = nums[i]
                 count+=1
                 
-        while count < n:
+        while count < len(nums):
             nums[count] = 0
             count += 1
         
