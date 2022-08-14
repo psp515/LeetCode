436e9fd51c04406ad26348bc50896c6b73b30ba2
Łukasz Kolber <69080157+psp515@users.noreply.github.com>
2022-08-14 13:52:37 +0200

    Time: 124 ms (48.33%), Space: 13.6 MB (74.05%) - LeetHub

---
+++ 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.py
@@ -0,0 +1,32 @@
+class Solution(object):
+    def findMedianSortedArrays(self, A, B):
+        n, m = len(A), len(B)
+        
+        if (n > m):
+            return self.findMedianSortedArrays(B, A)  
+ 
+        start = 0
+        end = n
+        realmidinmergedarray = (n + m + 1) // 2
+ 
+        while (start <= end):
+            mid = (start + end) // 2
+            leftAsize = mid
+            leftBsize = realmidinmergedarray - mid
+             
+            leftA = A[leftAsize - 1] if (leftAsize > 0) else float('-inf')
+            leftB = B[leftBsize - 1] if (leftBsize > 0) else float('-inf')
+            rightA = A[leftAsize] if (leftAsize < n) else float('inf')
+            rightB = B[leftBsize] if (leftBsize < m) else float('inf')
+ 
+            if leftA <= rightB and leftB <= rightA:
+                if ((m + n) % 2 == 0):
+                    return (max(leftA, leftB) + min(rightA, rightB)) / 2.0
+                return max(leftA, leftB)
+ 
+            elif (leftA > rightB):
+                end = mid - 1
+            else:
+                start = mid + 1
+            
+            
