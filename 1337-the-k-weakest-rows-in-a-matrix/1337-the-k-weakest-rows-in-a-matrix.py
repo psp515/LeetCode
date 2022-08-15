b2a785b6efa2731ed9937d95ffbf5b1c6dc46298
Łukasz Kolber <69080157+psp515@users.noreply.github.com>
2022-08-15 16:37:46 +0200

    Time: 133 ms (53.93%), Space: 13.7 MB (44.38%) - LeetHub

---
+++ 1337-the-k-weakest-rows-in-a-matrix/1337-the-k-weakest-rows-in-a-matrix.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def kWeakestRows(self, mat, k):
+        n = len(mat)
+        f = [(sum(mat[i]), i) for i in range(n)]  
+        
+        f.sort(key = lambda x:x[1])
+        f.sort(key = lambda x:x[0])
+        f = [f[i][1] for i in range(n)]
+        return f[:k]
+        
