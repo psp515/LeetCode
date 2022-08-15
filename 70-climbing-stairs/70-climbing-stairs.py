09ae6a41e036e7c62defd4d0e3302a7712dadcac
Łukasz Kolber <69080157+psp515@users.noreply.github.com>
2022-08-15 13:08:29 +0200

    Time: 33 ms (20.10%), Space: 13.3 MB (85.36%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.py
+++ 70-climbing-stairs/70-climbing-stairs.py
@@ -4,12 +4,10 @@ class Solution(object):
             return 1
         
         # f[i] - liczba sposobów wejswcia na i-ty schód
-        f = [float('inf') for _ in range(n)]
-        f[0] = 1
-        f[1] = 2
+        act,prev = 2, 1
         for i in range(2, n):
-            f[i] = f[i-1] + f[i-2]
+            act,prev = act+ prev, act
         
-        return f[-1]
+        return act
         
         
