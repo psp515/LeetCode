159fa085f738f0f977aa55865f9367d62670285f
Łukasz Kolber <69080157+psp515@users.noreply.github.com>
2022-08-15 17:44:19 +0200

    Time: 134 ms (54.19%), Space: 16.9 MB (59.57%) - LeetHub

--- 347-top-k-frequent-elements/347-top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.py
@@ -1,17 +1,30 @@
 class Solution(object):
     def topKFrequent(self, nums, k):
-        const = 10 ** 4
-        buckets = [[0, i - const] for i in range(2 * const + 1)]
+        d = {}
 
         for element in nums:
-            index = element + const
-            buckets[index][0] += 1
+            if element in d:
+                d[element] += 1
+            else:
+                d[element] = 1
 
-        buckets.sort(key=lambda x: x[0], reverse=True)
+        data = list(d.items())
+        data.sort(key=lambda x: x[1], reverse=True)
 
-        values = []
+        data = [data[i][0] for i in range(len(data))]
 
-        for i in range(k):
-            values.append(buckets[i][1])
-
-        return values
+        return data[:k]
+        
+        
+    
+    #def topKFrequent(self, nums, k):
+    #    const = 10 ** 4
+    #    buckets = [[0, i - const] for i in range(2 * const + 1)]
+    #    for element in nums:
+    #        index = element + const
+    #        buckets[index][0] += 1
+    #    buckets.sort(key=lambda x: x[0], reverse=True)
+    #    values = []
+    #    for i in range(k):
+    #        values.append(buckets[i][1])
+    #    return values
